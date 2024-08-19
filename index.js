390d76d9b50d3758c8a8902158ca7f264f825741
Samuel-Oyelaran <samueloyelaran4@gmail.com>
2024-08-19 12:48:47 +0000

    Just skimming through js

---
+++ index.js
@@ -0,0 +1,107 @@
+//  console.log ("Hello World!");
+//  let User = 'John';
+//  let Age = 25;
+//  let message = "Hello";
+//  let secondMessage = message;
+//  console.log (message);
+//  console.log (secondMessage);
+// message = "stronghead";
+// console.log (message);
+// console.log (secondMessage)
+
+// if (true){
+//     var test = false;
+    
+// }
+// console.log (test);
+
+// if (true){
+//     let testingPage = true;
+// }
+// console.log (testingPage);
+
+
+// Exponentation in Js with ** and Math.pow commands
+/**let entity1 = 5;
+let entity2 = 2;
+let entity3 = Math.pow (entity1, entity2);
+console.log(entity3)
+
+let entity4 = Math.pow (entity3, -2);
+console.log (entity4);
+
+let entity5 = entity3 ** 2;
+console.log (entity5);**/
+
+// String Operator in js
+// var age = 25;
+// var myInfo = "I am " + age + " years old";
+// console.log (myInfo);
+
+// //conditional statements in js
+// const x = 15;
+// const y = 20;
+// if (x==10){
+//     console.log ("if!");
+//     elseif ((x==15)) {
+//         console.log ("else if");
+   
+//     }
+// }
+// let n = 2;
+
+// n *= 3 + 5;//
+
+// n-=10*2;//
+
+
+// console.log (n);
+
+// Increent and Decrement operators
+let counter = 2;
+alert (counter++);
+alert (counter);
+
+/**Comma Operator**/
+// let a = (1 + 2, 3 + 4);
+
+// console.log (a);//7
+
+
+
+// let age = prompt('How old are you?', 100);
+
+// alert(`You are ${age} years old!`);
+
+// alert(`You are ${age} years old!`); // You are 100 years old!
+// let isBoss = confirm("Are you the boss?");
+// alert( isBoss);
+
+/*Creating a web-page that asks for a name and outputs it*/
+// let firstName = prompt ("What is your name?");
+// alert (`You are ${firstName}`);
+
+/*Comparison Operators*/
+// String Comparisons
+/*alert ('z' > 'a');
+alert ('Glowing' > 'Glower');
+alert ('Tunde' > "Seyi")
+let tunde =5;
+let samuel =4;
+alert (samuel > tunde);
+alert (tunde!=samuel);*/
+
+//Comparison of Different types 
+
+// alert (true == 1);
+// alert (false == 0);
+
+//Comparison with null and undefined
+alert (null === undefined);
+alert (null == undefined);
+
+
+
+
+
+
