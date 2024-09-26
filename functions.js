fbd031b20d7b9182e9edfa8c39202c488f92c2b7
Samuel-Oyelaran <samueloyelaran4@gmail.com>
2024-09-26 19:26:28 +0000

    the last bit for the day

--- functions.js
+++ functions.js
@@ -1,5 +1,19 @@
-// This is the beginning of our function journey
-function whenAreWeGoingHome() {
-    console.log("We are going home by 9pm today!");
+
+// Functions in JavaScript
+// functions without parameters
+// function whenAreWeGoingHome() {
+//     console.log("We are going home by 9pm today!");
+// }
+// whenAreWeGoingHome();
+
+// Playing with function scope
+let userName = "John";
+function showMessage() {
+    userName = "Bob";
+    let message = "Hello, " + userName;
+    console.log(message);
 }
-whenAreWeGoingHome();
+console.log(userName);
+showMessage();
+console.log(userName);
+console.log (userName);
